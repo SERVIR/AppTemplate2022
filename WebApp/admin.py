32acb282af9556363b721a319e0ecc1a0fff7b48
gtondapu <githika.tondapu@nasa.gov>
2022-11-24 14:58:37 -0500

    updates to data model

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from WebApp.models import Station, Measurement, Organization
+
+admin.site.register(Station)
+admin.site.register(Measurement)
+admin.site.register(Organization)
