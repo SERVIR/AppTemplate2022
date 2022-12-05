741eaa23c3ec0c26f26579ab647000a39738e076
SERVIR <SERVIRglobal@users.noreply.github.com>
2022-12-05 16:54:16 -0600

    Modifications to Admin Pages
    Added list_diplay, list_filter, search_fields and date_hierarchy to models
    Added Admin Site header

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from WebApp.models import Station, Measurement, Organization
 from import_export.admin import ImportExportModelAdmin
 
+admin.site.site_header = "SERVIR Template App Administration"
+
 admin.site.register(Station)
 admin.site.register(Organization)
 
@@ -11,5 +13,20 @@ class MeasurementAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('station', 'measurement_date', 'measurement_temp','measurement_precip')
     list_filter = ('station__station_name',)
     search_fields = ['station__station_name',]
+    date_hierarchy = 'measurement_date'
 
 admin.site.register(Measurement, MeasurementAdmin)
+
+class StationAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('station_id', 'station_name', 'station_location')
+    list_filter = ('station_organization__organization_name', 'station_organization__organization_country')
+    search_fields = ['station_organization__organization_name', 'station_organization__organization_country', 'station_organization__organization_city' ]
+
+admin.site.register(Station, StationAdmin)
+
+class OrganizationAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('organization_id', 'organization_name', 'organization_country')
+    list_filter = ('organization_country', 'organization_city')
+    search_fields = ['organization_name', 'organization_country', 'organization_city' ]
+
+admin.site.register(Organization, OrganizationAdmin)
