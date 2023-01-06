6e7c1f954ffcf11c4b83fe041824a7cc97912732
gtondapu <githika.tondapu@nasa.gov>
2023-01-06 12:39:48 -0500

    gee info button

---
+++ staticfiles/js/js/map_fullscreen.js
@@ -0,0 +1,219 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+var map = L.map('map3', {
+    fullscreenControl: true,
+    timeDimension: true,
+    timeDimensionOptions: {
+        timeInterval: "2015-09-01/2015-09-03",
+        period: "PT1H",
+        currentTime: Date.parse("2015-09-01T00:00:00Z")
+    },
+    timeDimensionControl: true,
+    timeDimensionControlOptions: {
+        autoPlay: false,
+        loopButton: true,
+        timeSteps: 1,
+        playReverseButton: true,
+        limitSliders: true,
+        playerOptions: {
+            buffer: 0,
+            transitionTime: 250,
+            loop: true,
+        }
+    }, center: [42.35, -71.08], zoom: 3
+});
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+});
+let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+// create a satellite imagery layer
+let satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}');
+
+var baseMaps = {
+    "OpenStreetMap": streets,
+    "Satellite": satellite
+};
+osm.addTo(map);
+// L.Control.geocoder().addTo(map);
+
+
+
+var chirps = L.tileLayer.wms('https://thredds.servirglobal.net/thredds/wms/Agg/ucsb-chirps_global_0.05deg_daily.nc4', {
+    layers: 'precipitation_amount',
+    transparency: 'true',
+    format: 'image/png',
+    style: 'boxfill/apcp_surface',
+    maxZoom: 21,
+    zIndex: 400,
+    opacity: $("#opacity_chirps_full").val()
+});
+let esi = L.esri.dynamicMapLayer({
+    url: 'https://gis1.servirglobal.net/arcgis/rest/services/Global/ESI_4WK/MapServer',
+    transparency: 'true',
+    format: 'image/png',
+    style: 'boxfill/apcp_surface',
+    maxZoom: 21,
+    opacity: $("#opacity_esi_full").val()
+});
+
+var testTimeLayer = L.timeDimension.layer.wms(chirps, {
+    updateTimeDimension: true
+});
+$("#chirps_full").change(function () {
+    if (this.checked) {
+        console.log("checked");
+        // chirps.addTo(map);
+
+        testTimeLayer.addTo(map);
+        testTimeLayer.bringToFront();
+    } else {
+        testTimeLayer.remove();
+    }
+});
+
+$("#esi_full").change(function () {
+    if (this.checked) {
+
+
+        esi.addTo(map);
+        esi.bringToFront();
+
+    } else {
+        esi.remove();
+    }
+});
+
+$('#opacity_chirps_full').change(function () {
+    testTimeLayer.setOpacity($(this).val());
+});
+
+$('#opacity_esi_full').change(function () {
+    esi.setOpacity($(this).val());
+});
+
+// Finally append that node to the new parent, recursively searching out and re-parenting nodes.
+function setParent(el, newParent) {
+    newParent.appendChild(el);
+}
+
+// setParent(htmlObject, a);
+
+var control1 = L.Control.geocoder({collapsed: false});
+
+control1.addTo(map);
+
+
+let layerControlDiv = control1.getContainer();
+
+// you can set an id for it if you want to use it to override the CSS later
+layerControlDiv.setAttribute("id", "layer-control-id-full");
+
+let layerControlParentLayer = L.control({
+    position: "topright"
+});
+layerControlParentLayer.onAdd = function (map) {
+    // Create the main div that will hold all your elements
+    let parentDiv = L.DomUtil.create("a");
+
+    // you can set an id for it if you want to use it for CSS
+    parentDiv.setAttribute("id", "layer-control-parent-id-full");
+    parentDiv.appendChild(layerControlDiv);
+    L.DomEvent.disableClickPropagation(parentDiv);
+    return parentDiv;
+};
+// add the Layer to the map
+layerControlParentLayer.addTo(map);
+var htmlObject = layerControlParentLayer.getContainer();
+var a = document.getElementById('location_full');
+setParent(htmlObject, a);
+
+var terrainLayer = L.tileLayer(
+    "https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}",
+    {
+        attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 0,
+        maxZoom: 22,
+        subdomains: 'abcd',
+        accessToken: 'rU9sOZqw2vhWdd1iYYIFqXxstyXPNKIp9UKC1s8NQkl9epmf0YpFF8a2HX1sNMBM',
+        opacity: 1,
+        thumb: "img/terrain.png",
+        displayName: "Terrain",
+    }
+);
+var deLormeLayer = L.tileLayer.wms(
+    "https://server.arcgisonline.com/arcgis/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}",
+    {
+        format: "image/png",
+        transparent: true,
+        attribution:
+            'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' +
+            'rest/services/Reference/Specialty/DeLorme_World_Base_Map/MapServer">ArcGIS</a>',
+        opacity: 1,
+        thumb: "img/delorme.png",
+        displayName: "DeLorme",
+    }
+);
+var gSatLayer = L.tileLayer(
+    "https://{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+    {
+        format: "image/png",
+        transparent: true,
+        attribution:
+            'Tiles © Map data ©2019 Google',
+        opacity: 1,
+        thumb: "img/gsatellite.png",
+        displayName: "Google Satellite",
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    }
+);
+
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+
+removeLayers = function () {
+    satellite.remove();
+    osm.remove();
+    OpenTopoMap.remove();
+    terrainLayer.remove();
+    deLormeLayer.remove();
+    gSatLayer.remove();
+}
+
+add_basemap = function (map_name) {
+    removeLayers();
+
+    switch (map_name) {
+        case "osm":
+
+            osm.addTo(map);
+            // osm.bringToFront();
+
+            break;
+        case "delorme":
+            deLormeLayer.addTo(map);
+            break;
+        case "satellite":
+            satellite.addTo(map);
+            break;
+
+        case "terrain":
+            terrainLayer.addTo(map);
+            break;
+        case "topo":
+            OpenTopoMap.addTo(map);
+            break;
+        case "gsatellite":
+            gSatLayer.addTo(map);
+            break;
+        default:
+            osm.addTo(map);
+
+    }
+}
