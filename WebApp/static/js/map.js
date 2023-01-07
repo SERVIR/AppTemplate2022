00154e2a661c97376459f25b43f8bc3f3c77f9b4
Billy Ashmall <billy.ashmall@nasa.gov>
2023-01-07 13:42:23 -0600

    map_fixedsize extend base

--- WebApp/static/js/map.js
+++ WebApp/static/js/map.js
@@ -1,10 +1,10 @@
- // initialize the map
-  var map = L.map('map').setView([42.35, -71.08], 13);
+// initialize the map
+var map = L.map('map').setView([42.35, -71.08], 13);
 
-  // load a tile layer
-  L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+// load a tile layer
+L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
     {
-      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-      maxZoom: 17,
-      minZoom: 9
+        attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+        maxZoom: 17,
+        minZoom: 9
     }).addTo(map);
