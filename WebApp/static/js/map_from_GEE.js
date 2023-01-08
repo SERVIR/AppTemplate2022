dedce8cc140eced7879c1197eed2cef5dc327944
Billy Ashmall <billy.ashmall@nasa.gov>
2023-01-08 11:25:46 -0600

    gee legend example

--- WebApp/static/js/map_from_GEE.js
+++ WebApp/static/js/map_from_GEE.js
@@ -214,3 +214,66 @@ add_basemap = function (map_name) {
 
     }
 }
+
+function add_legend(){
+
+    // append a defs (for definition) element to your SVG
+    var svgLegend = d3.select('#legend').append('svg');
+    var defs = svgLegend.append('defs');
+
+    // append a linearGradient element to the defs and give it a unique id
+    var linearGradient = defs.append('linearGradient')
+        .attr('id', 'linear-gradient');
+
+    // horizontal gradient
+    linearGradient
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "0%");
+
+    // append multiple color stops by using D3's data/enter step
+    linearGradient.selectAll("stop")
+        .data([
+            {offset: "0%", color: "#1303ff"},
+            {offset: "33%", color: "#42fff6"},
+            {offset: "66%", color: "#f3ff40"},
+            {offset: "100%", color: "#ff5d0f"}
+        ])
+        .enter().append("stop")
+        .attr("offset", function(d) {
+            return d.offset;
+        })
+        .attr("stop-color", function(d) {
+            return d.color;
+        });
+
+    // append title
+    svgLegend.append("text")
+        .attr("class", "legendTitle")
+        .attr("x", 0)
+        .attr("y", 20)
+        .style("text-anchor", "left")
+        .text("Legend title");
+
+    // draw the rectangle and fill with gradient
+    svgLegend.append("rect")
+        .attr("x", 0)
+        .attr("y", 30)
+        .attr("width", 300)
+        .attr("height", 15)
+        .style("fill", "url(#linear-gradient)");
+
+    //create tick marks
+    var xLeg = d3.scaleLinear()
+        .domain([258, 316])
+        .range([0, 300]);
+
+    var axisLeg = d3.axisBottom(xLeg);
+
+    svgLegend
+        .attr("class", "axis")
+        .append("g")
+        .attr("transform", "translate(0, 40)")
+        .call(axisLeg);
+}
