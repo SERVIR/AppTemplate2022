a0a151b0a165ffc80c7b909571bcaa641d849069
Billy Ashmall <billy.ashmall@nasa.gov>
2023-01-09 11:23:08 -0600

    steeings

--- SERVIR_AppTemplate/settings.py
+++ SERVIR_AppTemplate/settings.py
@@ -57,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_DIRS= [
+    BASE_DIR / "WebApp/static"
+    ]
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
